ba77af11f37265ca8bfc9491c1d2991b22330c10
Richard Kiss <him@richardkiss.com>
2019-02-09 21:30:40 -0800

    Add MANIFEST.in so it actually installs. Version bump.

--- aiter/__init__.py
+++ aiter/__init__.py
@@ -1,12 +1,15 @@
-__version__ = "0.1"
+__version__ = "0.1.2"
 
+from .active_aiter import active_aiter  # noqa
 from .aiter_forker import aiter_forker  # noqa
+from .aiter_to_iter import aiter_to_iter  # noqa
 from .azip import azip  # noqa
 from .flatten_aiter import flatten_aiter  # noqa
 from .gated_aiter import gated_aiter  # noqa
 from .iter_to_aiter import iter_to_aiter  # noqa
 from .join_aiters import join_aiters  # noqa
 from .map_aiter import map_aiter  # noqa
+from .map_filter_aiter import map_filter_aiter  # noqa
 from .parallel_map_aiter import parallel_map_aiter  # noqa
 from .preload_aiter import preload_aiter  # noqa
 from .push_aiter import push_aiter  # noqa
