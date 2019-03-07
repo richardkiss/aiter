d27a8ecf4ff267f4124b1cbf47405f10308df5aa
Richard Kiss <him@richardkiss.com>
2019-03-07 11:23:12 +0900

    Rewrite aiter_forker and add deferred_coroutine.

--- aiter/aiter_forker.py
+++ aiter/aiter_forker.py
@@ -1,6 +1,29 @@
-import asyncio
+from .deferred_coroutine import deferred_coroutine
 
-from .push_aiter import push_aiter
+
+class _aiter_fork:
+    """
+    Implementation of an aiter fork. Traces through a linked list of aiter elements, waiting
+    when necessary.
+    """
+    def __init__(self, next_awaitable, is_active=False):
+        self._next_awaitable = next_awaitable
+        self._is_active = is_active
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        this_item, next_awaitable = await self._next_awaitable.wait(is_active=self._is_active)
+        self._next_awaitable = next_awaitable
+        return this_item
+
+    def fork(self, is_active=True):
+        """
+        Create a new fork: either active, which uses the current task to await the next
+        item; or passive, which waits until an active fork awaits it.
+        """
+        return _aiter_fork(self._next_awaitable, is_active=is_active)
 
 
 def aiter_forker(aiter):
@@ -9,40 +32,22 @@ def aiter_forker(aiter):
     iterators, each of which produces the same elements), wrap it with this
     function.
 
-    Returns a :class:`aiter.push_aiter <push_aiter>` object that will yield
+    Returns a :class:`aiter._aiter_fork <_aiter_fork>` object that will yield
     the same objects in the same order. This object supports
-    :py:func:`fork <aiter.push_aiter.fork>`, which will let you create a
+    :py:func:`fork <aiter._aiter_fork.fork>`, which will let you create a
     duplicate stream.
 
     :type aiter: aiter
     :param aiter: an async iterator
 
-    :return: a :class:`aiter.push_aiter <push_aiter>`
-    :rtype: :class:`aiter.push_aiter <push_aiter>`
+    :return: a :class:`aiter._aiter_fork <_aiter_fork>`
+    :rtype: :class:`aiter._aiter_fork <_aiter_fork>`
     """
+    _open_aiter = aiter.__aiter__()
 
-    open_aiter = aiter.__aiter__()
+    async def get_next():
+        next_item = await _open_aiter.__anext__()
+        return next_item, deferred_coroutine(get_next)
 
-    async def worker(open_aiter, pa):
-        try:
-            _ = await open_aiter.__anext__()
-            if not pa.is_stopped():
-                pa.push(_)
-        except StopAsyncIteration:
-            pa.stop()
-        pa.head().event.clear()
-
-    def make_kick():
-        def kick(pa):
-            event = pa.head().event
-            if event.is_set():
-                return
-            event.set()
-            pa.head().task = asyncio.ensure_future(worker(open_aiter, pa))
-        return kick
-
-    pa = push_aiter(next_preflight=make_kick())
-    pa.head().event = asyncio.Event()
-    pa.head().event.set()
-    pa.head().task = asyncio.ensure_future(worker(open_aiter, pa))
-    return pa
+    next_awaitable = deferred_coroutine(get_next)
+    return _aiter_fork(next_awaitable, is_active=True)
