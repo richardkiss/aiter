3ab3a5a5ea5c628230ddcef576bd30d5ab925496
Richard Kiss <him@richardkiss.com>
2019-12-04 14:45:08 -0800

    Fix comment in map_aiter.

--- aiter/map_aiter.py
+++ aiter/map_aiter.py
@@ -8,10 +8,8 @@ def map_aiter(map_f, aiter, worker_count=1):
     """
     Take an async iterator and a map function, and apply the function
     to everything coming out of the iterator before passing it on.
-    In this case, the map_f must return a list, which will be flattened.
-    Empty lists are okay, so you can filter items by excluding them from the list.
 
-    Note that since there are multiple workers, the order or processed elements
+    If there are multiple workers, the order or processed elements
     might not match the input order.
 
     :type aiter: async iterator
