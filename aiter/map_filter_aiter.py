a700ce7523c37fa3a702bbabe22e0ae48036b7b7
Richard Kiss <him@richardkiss.com>
2019-12-04 14:47:50 -0800

    Simplify map_filter_aiter.

--- aiter/map_filter_aiter.py
+++ aiter/map_filter_aiter.py
@@ -1,13 +1,18 @@
 import asyncio
 import logging
 
+from .flatten_aiter import flatten_aiter
+from .map_aiter import map_aiter
 
-async def map_filter_aiter(map_f, aiter):
+
+def map_filter_aiter(map_f, aiter, worker_count=1):
     """
     Take an async iterator and a map function, and apply the function
     to everything coming out of the iterator before passing it on.
     In this case, the map_f must return a list, which will be flattened.
     Empty lists are okay, so you can filter items by excluding them from the list.
+
+    Note: this simply calls map_aiter, then flatten_aiter.
 
     :type aiter: async iterator
     :param aiter: an aiter
@@ -16,19 +21,12 @@ async def map_filter_aiter(map_f, aiter):
         a list (or other iterable)
     :param map_f: the mapping function
 
+    :type worker_count: int
+    :param worker_count: the number of worker tasks that pull items out of aiter
+
     :return: an aiter returning transformed items that have been processed through map_f
     :rtype: an async iterator
     """
-    if asyncio.iscoroutinefunction(map_f):
-        _map_f = map_f
-    else:
-        async def _map_f(_):
-            return map_f(_)
 
-    async for _ in aiter:
-        try:
-            items = await _map_f(_)
-            for _ in items:
-                yield _
-        except Exception:
-            logging.exception("unhandled mapping function %s worker exception on %s", map_f, _)
+    new_aiter = flatten_aiter(map_aiter(map_f, aiter, worker_count))
+    return new_aiter
